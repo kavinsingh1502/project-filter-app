49fb7553ee13d2a6cfdfe58ab97619eded6e360b
kavinsingh1502 <75671178+kavinsingh1502@users.noreply.github.com>
2022-05-14 10:47:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload(){
+    moustache_nose=loadImage('https://i.postimg.cc/6qB4hVsN/m.png')
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+video.hide();
+video.size(300,300);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+    }
+}
+ function modelLoaded(){
+ console.log('poseNet is initialized');}
+
+ function draw(){
+    image(video,0,0,300,300);
+    }
+
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
